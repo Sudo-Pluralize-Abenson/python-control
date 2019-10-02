5e8f8c36fe845dded797394be460927993f64821
Rico Picone <ricopicone@gmail.com>
2019-10-02 16:30:10 -0700

    created class and added authors and date

--- control/design_region.py
+++ control/design_region.py
@@ -1,5 +1,38 @@
-"""desgin_region.py
+"""design_region.py - module for defining a complex plane dominant pole design region
 stuff stuff stuff. 
 It is designed for use in the python-control library.
 Routines in this module:
 """
+
+"""
+Authors: 
+Rico AR Picone
+Kelsey Buckles
+Alec Dryden
+Kenneth Echevaria
+Akara Hay
+Dane Webb
+
+Date (initial): 2 October 2019
+"""
+
+# external modules
+import numpy as np
+
+# variables to expose for import
+__all__ = ['design_region'] 
+
+# class definition!
+class design_region(
+    OS=np.nan, # defaults
+    Tr=np.nan, # defaults
+):
+  # class attributes go here
+  def __init__(self, OS, Tr):
+    # initialize instance attibutes
+    self.OS = OS
+    self.Tr = Tr
+
+  # methods
+  def update_attributes(attr):
+    print("updating attributes after setting " + attr)
