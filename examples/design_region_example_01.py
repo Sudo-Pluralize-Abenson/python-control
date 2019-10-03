f7655bedb3e49fe1be282cee40dbff4b8a8d706c
Rico Picone <ricopicone@gmail.com>
2019-10-03 16:20:50 -0700

    beginning examples

---
+++ examples/design_region_example_01.py
@@ -0,0 +1,24 @@
+""" design_region_example_01.py - using the design_region module and class
+
+Authors: Rico AR Picone
+Date (initial): 3 October 2019
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+sys.path.append(".") # Adds higher directory to python modules path.
+sys.path.append("..") # Adds higher directory to python modules path.import control as ct
+import control as ct
+
+# create design_region instance
+dr1 = ct.design_region()
+
+# print attributes and defaults
+print(dr1.__dict__)
+
+# set and print some parameters
+dr1.OS = 5.
+dr1.Tr = 1.3
+print(dr1.OS)
+print(dr1.Tr)
