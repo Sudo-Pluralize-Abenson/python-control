978b6f266f6a895790dbc6d7f8e3a2837d24bf92
Sudo-Pluralize-Abenson <65480364+Sudo-Pluralize-Abenson@users.noreply.github.com>
2020-08-03 17:26:32 -0700

    Updated Deprecated Features
    Sympy no longer supports * imports
    
    "import core" is now preferred.

--- examples/design_region_example_01.py
+++ examples/design_region_example_01.py
@@ -5,7 +5,6 @@ Date (initial): 3 October 2019
 """
 
 import numpy as np
-from sympy import *
 from sympy import core
 import matplotlib.pyplot as plt
 from scipy.constants import pi
@@ -35,6 +34,7 @@ print(dr1.dr_rt) # updated!
 print(dr1.r) # updated!
 print(dr1.theta) # updated!
 
+dr1.theta = [pi/2,pi]  #won't plot after this because we're not properly updating dr_xy
 
 print(dr1.dr_xy)
 
