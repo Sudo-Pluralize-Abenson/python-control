2d39d0b69ee282d419b42911f5d16436ee461bf2
Rico Picone <ricopicone@gmail.com>
2020-08-06 12:07:24 -0700

    reorganize and commented some stuff

--- examples/design_region_example_01.py
+++ examples/design_region_example_01.py
@@ -5,12 +5,10 @@ Date (initial): 3 October 2019
 """
 
 import numpy as np
-from sympy import core
+from sympy import *
 import matplotlib.pyplot as plt
-from scipy.constants import pi
 import sys
-sys.path.append(".") # Adds higher directory to python modules path.
-sys.path.append("..") # Adds higher directory to python modules path.import control as ct
+sys.path.insert(0,'../')
 import control as ct
 
 # create design_region instance
@@ -20,21 +18,29 @@ dr1 = ct.design_region()
 # print(dr1.__dict__)
 
 # set and print some parameters
-dr1.OS = 10
-dr1.Tr = [1.3,1.5]
-print(dr1.OS)
-print(dr1.Tr)
 
-print(dr1.dr_xy)
-print(dr1.dr_rt)
-dr1.x = [-4,-1]
-# dr1.y = [2,6]
-print(dr1.dr_xy) # updated!
-print(dr1.dr_rt) # updated!
-print(dr1.r) # updated!
-print(dr1.theta) # updated!
-
-print(dr1.dr_xy)
+# dr1.x = [-5,-2]
+dr1.t = [3*pi/4,7*pi/8]
+print(dr1.t)
 
 # plot design region
 dr1.plot_dr()
+
+# stuff that's broken
+
+# # projection stuff?
+# # dr1.theta = [pi/2,pi] # won't plot after this because we're not properly updating dr_xy
+# print(dr1.dr_xy)
+
+# # mixing r and theta stuff
+# print(dr1.dr_xy)
+# print(dr1.dr_rt)
+# dr1.x = [-4,-1]
+# # dr1.y = [2,6]
+# print(dr1.dr_xy) # updated!
+# print(dr1.dr_rt) # updated!
+# print(dr1.r) # updated!
+# print(dr1.theta) # updated!
+
+# # overshoot
+# dr1.OS = [10,20]
